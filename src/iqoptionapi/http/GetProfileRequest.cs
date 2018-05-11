cca71a73cd9dd034bf1e0058829eb91808af15a7
mongkoneiadon <mongkon.eiadon@hotmail.com>
2018-05-11 12:13:24 +0700

    add change nuget package

--- src/iqoptionapi/http/GetProfileRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿using iqoptionapi.http;
-
-namespace iqoptionapi.http {
-    public class GetProfileRequest : IqOptionRequest {
-        public GetProfileRequest():base("getprofile") { }
-    }
-}
---
+++ src/webapi/iqoption.web/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace iqoption.web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseApplicationInsights()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .ConfigureLogging((ctx, logging) => {
+                    logging.AddAzureWebAppDiagnostics();
+                    logging.AddEventSourceLogger();
+                    logging.AddConsole();
+                })
+                .Build();
+    }
+}
