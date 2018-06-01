dbae4d90b4c95363cd01b7fb2fe2a540ef481109
Mongkon Eiadon <liie.m@excelbangkok.com>
2018-06-01 18:27:44 +0700

    update to dotnet core 2.1

--- src/webapi/iqoption.web/Startup.cs
+++ src/webapi/iqoption.web/Startup.cs
@@ -30,6 +30,7 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace iqoption.web
 {
