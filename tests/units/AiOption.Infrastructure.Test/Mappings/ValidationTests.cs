93208e108a177b6927b736b69141b3d2a8587cf5
Lee Eiadon <leiadon@brandnewday.nl>
2018-08-17 19:37:12 +0700

    Add unit tests

---
+++ tests/units/AiOption.Infrastructure.Test/Mappings/ValidationTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using AutoMapper;
+
+using Xunit;
+
+namespace AiOption.Infrastructure.Test.Mappings
+{
+    public class ValidationTests
+    {
+        [Fact]
+        public void ValidateProfile() {
+
+            Mapper.Initialize(c => {
+                c.AddProfile<Infrastructure.Mappings.CustomersProfile>();
+
+            });
+
+            Mapper.AssertConfigurationIsValid();
+
+
+        }
+    }
+}
